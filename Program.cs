38bee460f6118ecacdf19c82afe0a2fc4b86e876
Mark Headd <mark.headd@gsa.gov>
2017-05-10 11:40:02 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using SoapHttpClient;
+using SoapHttpClient.Extensions;
+
+namespace soap_test
+{
+    class Program
+    {
+        private static XNamespace ns = XNamespace.Get("http://helio.spdf.gsfc.nasa.gov/");
+        private static string endpoint = "http://sscweb.gsfc.nasa.gov:80/WS/helio/1/HeliocentricTrajectoriesService";
+        private static SoapClient soapClient = new SoapClient();
+        static void Main(string[] args)
+        {
+            XElement body = new XElement(ns.GetName("getAllObjects"));
+            using (soapClient)
+            {
+                var result = soapClient.Post(
+                      endpoint: endpoint,
+                      body: body);
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(result.Content.ReadAsStringAsync().Result);
+                Console.WriteLine(JsonConvert.SerializeXmlNode(doc));
+            }
+        }
+    }
+    public class ContentTypeChangingHandler : DelegatingHandler
+    {
+        public ContentTypeChangingHandler(HttpMessageHandler innerHandler) : base(innerHandler) { }
+
+        protected async override Task<HttpResponseMessage> SendAsync(
+          HttpRequestMessage request,
+          CancellationToken cancellationToken)
+        {
+
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("text/xml; charset=utf-8");
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ README
@@ -0,0 +1,14 @@
+# .NET Core SOAP Test
+
+A simple C# console application to make a call to a SOAP web service and serialize the response as JSON.
+
+## Uses:
+
+* [SoapHttpClient](https://github.com/pmorelli92/SoapHttpClient)
+* [Json.NET](http://www.newtonsoft.com/json)
+
+Install Nuget packages in VS Code by doing:
+
+```bash
+~$ dotnet add package <package-name>
+```
